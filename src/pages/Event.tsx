2b02e31d0ee46a000f0e168699e7a9eed4e77f22
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-12-18 10:47:44 -0300

    feat: initial pages and components

---
+++ src/pages/Event.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header";
+import { Logo } from "../components/Logo";
+
+export function Event() {
+  return (
+    <header>
+      <Header />
+    </header>
+  )
+}
